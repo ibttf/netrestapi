6db5dfa6eaef13d156ee2c03fdc8ace095ceb609
roylee0912 <leeroy0912@gmail.com>
2023-04-06 10:05:11 -0400

    documentation on role class

--- Entities/Role.cs
+++ Entities/Role.cs
@@ -3,6 +3,7 @@ namespace WebApi.Entities;
 public enum Role
 {
     // validations, defines all available roles in the api. only admin and user.
+    //this is a separate class from string/int and is written this way to differentiate regular roles from just being strings.
     Admin,
     User
 }
